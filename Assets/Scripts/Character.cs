fbd9f4dddd12aa73b7d2616991bd2c5938d662ae
Emiliano-M-Salvado-F <emiliano.m.salvado.f@gmail.com>
2024-01-08 15:37:51 -0300

    in progress

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,4 +8,12 @@ public abstract class Character : MonoBehaviour
     [SerializeField] float _life;
     [SerializeField] Weapon _weapon;
     [SerializeField] Armor _armor;
+
+    public void TakeDamage(float dmg)
+    {
+        _life -= dmg;
+        if (_life <= 0) Die();
+    }
+
+    public abstract void Die();
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,8 @@ public abstract class Weapon : MonoBehaviour
     [SerializeField] string _name;
     [SerializeField] AttackType _attackType;
     [SerializeField] AttackEffect _attackEffect;
+    [SerializeField] Class _strongClass;
+    [SerializeField] Class _weakClass;
     [SerializeField][Range(0f,1f)] float _effectChance;
     [SerializeField] float _baseDamage;
 }
